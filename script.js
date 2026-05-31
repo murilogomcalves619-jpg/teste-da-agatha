fe7c5b5486108d855afd5ccf70cf7837200fe060
murilogomcalves619-jpg <murilogomcalves619@gmail.com>
2026-05-31 19:17:57 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -10,20 +10,3 @@ function mostrarMensagem() {
     const aleatoria = Math.floor(Math.random() * mensagens.length);
     document.getElementById("mensagem").textContent = mensagens[aleatoria];
 }
-
-const express = require("express");
-const path = require("path");
-
-const app = express();
-
-app.use(express.static(__dirname));
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
