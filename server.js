9909790ea30143650a158375895eeb63d60f92d7
murilogomcalves619-jpg <murilogomcalves619@gmail.com>
2026-05-31 19:44:12 -0300

    sexto commit

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        status: "OK",
+        uptime: process.uptime()
+    });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
